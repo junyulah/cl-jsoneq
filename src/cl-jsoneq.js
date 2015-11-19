dee4558557cf27d193aad1c7a29fbe40da920d51
ddchen <842914439@qq.com>
2015-11-19 23:02:43 +0800

    base

--- src/cl-jsoneq.js
+++ src/cl-jsoneq.js
@@ -1,1 +1,62 @@
-module.exports = () => {}
+let jsonEq = (json1, json2) => {
+    if (sameType(json1, json2) === 'array') {
+        if (json1.length !== json2.length)
+            return false;
+        for (let i = 0; i < json1.length; i++) {
+            if (!jsonEq(json1[i], json2[i]))
+                return false
+        }
+        return true;
+    } else if (sameType(json1, json2) === 'map') {
+        let keys1 = getKeys(json1);
+        let keys2 = getKeys(json2);
+        if (keys1.length !== keys2.length)
+            return false;
+        for (let i = 0; i < keys1.length; i++) {
+            let key = keys1[i];
+            if (json2[key] === undefined)
+                return false;
+            if (!jsonEq(json1[key], json2[key]))
+                return false;
+        }
+        return true;
+    } else if (sameType(json1, json2) === 'atom') {
+        return json1 === json2;
+    }
+    return false;
+}
+
+let getKeys = (map) => {
+    if (Object.keys) return Object.keys(map);
+    let keys = [];
+    for (let name in map) {
+        keys.push(name);
+    }
+    return keys;
+}
+
+let sameType = (v1, v2) => {
+    let type1 = getType(v1);
+    let type2 = getType(v2);
+    if (type1 === type2)
+        return type1
+    return false;
+}
+
+let getType = (v) => {
+    if (isArray(v)) return 'array';
+    else if (isMap(v)) return 'map';
+    else if (isAtom(v)) return 'atom';
+    throw new TypeError('Expect Array or Map or Atom type. Instead got ' + v);
+}
+
+let isArray = v => v && typeof v === 'object' && typeof v.length === 'number';
+
+let isMap = v => v && typeof v === 'object' && typeof v.length !== 'number';
+
+let isAtom = v => v === null ||
+    typeof v === 'string' ||
+    typeof v === 'number' ||
+    typeof v === 'boolean'
+
+module.exports = jsonEq;
--- test/base.js
+++ test/base.js
@@ -1,8 +1,133 @@
 import assert from 'assert';
-import index from '../index';
+import jsonEq from '../index';
 
 describe('base', () => {
-  it('base', () => {
+    it('base', () => {
+        assert.equal(jsonEq(1, 1), true);
+        assert.equal(jsonEq(1, 2), false);
 
-  });
+        assert.equal(jsonEq('abc', 'abc'), true);
+        assert.equal(jsonEq('abdc', 'abc'), false);
+
+        assert.equal(jsonEq(null, null), true);
+
+        assert.equal(jsonEq(true, true), true);
+        assert.equal(jsonEq(false, false), true);
+        assert.equal(jsonEq(true, false), false);
+
+        assert.equal(jsonEq(1, '1'), false);
+        assert.equal(jsonEq(0, false), false);
+        assert.equal(jsonEq(null, 0), false);
+    });
+
+    it('arr', () => {
+        assert.equal(jsonEq([], []), true);
+        assert.equal(jsonEq([1, 2, 3], [1, 2, 3]), true);
+        assert.equal(jsonEq([1, 2, 3], [3, 2, 1]), false);
+        assert.equal(jsonEq([1, 2, 3], [1, 2]), false);
+    });
+
+    it('map', () => {
+        assert.equal(jsonEq({}, {}), true);
+        assert.equal(jsonEq({
+            a: 1,
+            b: 2
+        }, {
+            a: 1,
+            b: 2
+        }), true);
+
+        assert.equal(jsonEq({
+            a: 1,
+            b: 2
+        }, {
+            a: 1,
+            b: 3
+        }), false);
+
+        assert.equal(jsonEq({
+            a: 1,
+            b: 2,
+            c: 3
+        }, {
+            a: 1,
+            b: 2
+        }), false);
+
+        assert.equal(jsonEq({
+            a: 1,
+            b: 2
+        }, {
+            a: 1
+        }), false);
+
+        assert.equal(jsonEq({
+            a: 1,
+            b: 2
+        }, {
+            c: 1,
+            d: 2
+        }), false);
+    });
+
+    it('com', () => {
+        assert.equal(jsonEq({
+            a: [1, 2],
+            b: {
+                c: null,
+                d: false,
+                e: {
+                    f: 'ok'
+                }
+            }
+        }, {
+            a: [1, 2],
+            b: {
+                c: null,
+                d: false,
+                e: {
+                    f: 'ok'
+                }
+            }
+        }), true);
+
+        assert.equal(jsonEq({
+            a: [1, 2],
+            b: {
+                c: null,
+                d: false,
+                e: {
+                    f: 'ok'
+                }
+            }
+        }, {
+            a: [1, 2],
+            b: {
+                c: null,
+                d: false,
+                e: {
+                    f: 'ok',
+                    g: 'fine'
+                }
+            }
+        }), false);
+    });
+
+    it('exception', (done) => {
+        try {
+            assert.equal(jsonEq(null, undefined), true);
+        } catch (err) {
+            assert.equal(err.toString().indexOf('TypeError') !== -1, true);
+            done();
+        }
+    });
+
+    it('exception2', (done) => {
+        try {
+            assert.equal(jsonEq(null, function() {}), true);
+        } catch (err) {
+            assert.equal(err.toString().indexOf('TypeError') !== -1, true);
+            done();
+        }
+    });
 });
